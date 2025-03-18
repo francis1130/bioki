3b218827437bc62c8c7cca798a581d315241e128
Davi Gomes da Silva <davi46796@gmail.com>
2025-03-18 14:27:04 -0300

    commit bioki

---
+++ bioki.py
@@ -0,0 +1,9 @@
+#Feito em grupo: Gabriel Francisco, Raquel Pinheiro e João Regis
+dia = (input("Diga o dia da história: "))
+person = (input("Fale o nome do protagonista da história: "))
+acao = (input("Diga o que ele estava fazendo com o cachorro: "))
+dog = (input("Qual o nome do cachorro?: "))
+lugar = (input("Diga para onde ele foi: "))
+lugarquemora = (input("Diga onde ele mora: "))
+
+print(f"Na madrugada do dia {dia}, {person} estava {acao} com {dog}, seu cachorro. Após isso, foi ao {lugar} e depois para casa, em {lugarquemora}.")
